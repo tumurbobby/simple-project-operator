a99f6237fa4500118b4f7966bb1bfa0f8f94b074
tumurbobby <bobbytumur@gmail.com>
2026-07-19 18:03:17 +0900

    refactor: remove commented-out resource quota creation code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/kubernetes"
@@ -30,29 +29,45 @@ func main() {
 	// 		Name: "bobby-demo",
 	// 	},
 	// }
-	rq := &corev1.ResourceQuota{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "default-quota",
-			Namespace: "bobby-demo",
-		},
-		Spec: corev1.ResourceQuotaSpec{
-			Hard: corev1.ResourceList{
-				corev1.ResourcePods:   resource.MustParse("2"),
-				corev1.ResourceCPU:    resource.MustParse("500m"),
-				corev1.ResourceMemory: resource.MustParse("500Mi"),
-			},
-		},
-	}
-	_, err = clientset.CoreV1().
-		ResourceQuotas(rq.Namespace).
-		Create(
+	// rq := &corev1.ResourceQuota{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name:      "default-quota",
+	// 		Namespace: "bobby-demo",
+	// 	},
+	// 	Spec: corev1.ResourceQuotaSpec{
+	// 		Hard: corev1.ResourceList{
+	// 			corev1.ResourcePods:   resource.MustParse("2"),
+	// 			corev1.ResourceCPU:    resource.MustParse("500m"),
+	// 			corev1.ResourceMemory: resource.MustParse("500Mi"),
+	// 		},
+	// 	},
+	// }
+	// _, err = clientset.CoreV1().
+	// 	ResourceQuotas(rq.Namespace).
+	// 	Create(
+	// 		context.Background(),
+	// 		rq,
+	// 		metav1.CreateOptions{},
+	// 	)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	watcher, err := clientset.CoreV1().
+		Namespaces().
+		Watch(
 			context.Background(),
-			rq,
-			metav1.CreateOptions{},
+			metav1.ListOptions{},
 		)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Resource Quota created!")
+	for event := range watcher.ResultChan() {
+
+		ns := event.Object.(*corev1.Namespace)
+
+		fmt.Println(event.Type, ns.Name)
+
+	}
 }
