cba4afce83d38f827b17efbc6634c01137d3af89
tumurbobby <bobbytumur@gmail.com>
2026-07-20 14:20:56 +0900

    refactor: update namespace watcher to use in-cluster config and clean up code

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"path/filepath"
+	"fmt"
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
@@ -11,12 +11,12 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
-	// Load kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join("/home/vagrant/.kube", "config"))
+	// Load sa
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err)
 	}
@@ -26,54 +26,54 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// ns := &corev1.Namespace{
-	// 	ObjectMeta: metav1.ObjectMeta{
-	// 		Name: "bobby-demo",
-	// 	},
-	// }
 
-	watcher, err := clientset.CoreV1().
-		Namespaces().
-		Watch(
-			context.Background(),
-			metav1.ListOptions{},
-		)
-	if err != nil {
-		panic(err)
-	}
-
-	for event := range watcher.ResultChan() {
-
-		ns := event.Object.(*corev1.Namespace)
-		if !strings.HasPrefix(ns.Name, "bobby-") {
-			continue
+	for {
+		watcher, err := clientset.CoreV1().
+			Namespaces().
+			Watch(
+				context.Background(),
+				metav1.ListOptions{},
+			)
+		if err != nil {
+			panic(err)
 		}
-		// fmt.Println(event.Type, ns.Name)
-		if event.Type == watch.Added {
-			rq := &corev1.ResourceQuota{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "default-quota",
-					Namespace: ns.Name,
-				},
-				Spec: corev1.ResourceQuotaSpec{
-					Hard: corev1.ResourceList{
-						corev1.ResourcePods:   resource.MustParse("2"),
-						corev1.ResourceCPU:    resource.MustParse("500m"),
-						corev1.ResourceMemory: resource.MustParse("500Mi"),
+		for event := range watcher.ResultChan() {
+
+			ns, ok := event.Object.(*corev1.Namespace)
+			if !ok {
+				continue
+			}
+			if !strings.HasPrefix(ns.Name, "bobby-") {
+				continue
+			}
+			if event.Type == watch.Added {
+				rq := &corev1.ResourceQuota{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "default-quota",
+						Namespace: ns.Name,
 					},
-				},
+					Spec: corev1.ResourceQuotaSpec{
+						Hard: corev1.ResourceList{
+							corev1.ResourcePods:   resource.MustParse("2"),
+							corev1.ResourceCPU:    resource.MustParse("500m"),
+							corev1.ResourceMemory: resource.MustParse("500Mi"),
+						},
+					},
+				}
+				_, err = clientset.CoreV1().
+					ResourceQuotas(rq.Namespace).
+					Create(
+						context.Background(),
+						rq,
+						metav1.CreateOptions{},
+					)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
 			}
-			_, err = clientset.CoreV1().
-				ResourceQuotas(rq.Namespace).
-				Create(
-					context.Background(),
-					rq,
-					metav1.CreateOptions{},
-				)
-			if err != nil {
-				panic(err)
-			}
+			fmt.Println("Watch ended, reconnecting...")
 		}
-
 	}
+
 }
